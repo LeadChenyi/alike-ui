c4f810cb81ec7fe2a48bef479837613f6416096b
Charleyi <735767177@qq.com>
2020-10-04 00:00:31 +0800

    Add  实现左滑右滑

--- packages/swiper-action/swiper-action-item.vue
+++ packages/swiper-action/swiper-action-item.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="alike-swiper-action-item" @mousedown="mousedown">
-        <div class="alike-swiper-action-section">
+        <div class="alike-swiper-action-section" :style="{transform:`translate3d(${translateX}px,0,0)`}">
             <slot></slot>
         </div>
         <div class="alike-swiper-action-footer">
@@ -13,9 +13,63 @@
 <script>
 export default {
     name:"alike-swiper-action-item",
+    data(){
+        return {
+            translateX:0,       // 当前值     
+            thresholdX:40,      // 门槛
+            limitX:120,         // 限制
+            recordX:0,          // 记录值
+            direction:"left"
+        }
+    },
     methods:{
         mousedown(e){
-            console.log(e);
+            let downX = e.clientX;
+
+            document.onmousemove = (e)=>{
+                let moveX = e.clientX;
+
+                /**
+                 * 记录值每次是会重新计算的
+                 * 左滑：当前值 = 记录值
+                 * 右滑：当前值 + 记录值
+                 */
+                this.recordX = moveX - downX;
+
+                if(this.recordX <= 0){
+                    if(Math.abs(this.recordX) >= this.limitX){
+                        this.recordX = -this.limitX;
+                    }
+                    this.translateX = this.recordX;
+                    this.direction = "left";
+                    // console.log('向左滑...',this.recordX);
+                }else{
+                    this.translateX += this.recordX;
+                    if(this.translateX > 0){
+                        this.translateX = 0
+                    }
+                    this.direction = "right";
+                    // console.log('向右滑...',this.recordX);
+                }
+            }
+
+            document.onmouseup = ()=>{
+                if(this.direction == "left"){
+                    if(Math.abs(this.recordX) >= this.thresholdX){
+                        this.translateX = -this.limitX;
+                    }else{
+                        this.translateX = 0;
+                    }
+                }else{
+                    if(Math.abs(this.translateX) < (this.limitX - this.thresholdX)){
+                        this.translateX = 0;
+                    }else{
+                        this.translateX = -this.limitX;
+                    }
+                }
+                document.onmousemove = null;
+                document.onmouseup = null;
+            }
         }
     }
 }
@@ -23,9 +77,9 @@ export default {
 
 <style lang="scss" scoped>
     .alike-swiper-action-item{position:relative;overflow:hidden;display:flex;}
-    .alike-swiper-action-section{position:relative;flex:1;z-index:2;}
+    .alike-swiper-action-section{position:relative;flex:1;z-index:2;transition:transform 300ms ease;}
     .alike-swiper-action-footer{position:absolute;right:0;top:0;bottom:0;display:flex;}
-    .alike-swiper-action-btn{padding:0 15px;color:#ffffff;font-size:15px;}
+    .alike-swiper-action-btn{padding:0 15px;color:#ffffff;font-size:15px;line-height:40px;cursor:pointer;}
     .alike-swiper-action-btn--primary{background-color:$alike-color-primary;}
     .alike-swiper-action-btn--fail{background-color:$alike-color-fail;}
 </style>
