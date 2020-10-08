b8821639944b3261d29f002ec07edeac20b4dd49
Charleyi <735767177@qq.com>
2020-10-08 15:39:54 +0800

    Fix  自定义组件构建模块与jsx模块冲突

--- src/pages/jsx.vue
+++ src/pages/jsx.vue
@@ -1,16 +1,16 @@
 <script>
-// https://github.com/vuejs/jsx.
+// https://github.com/vuejs/jsx
 export default {
     name:"JSX",
     data(){
         return {
             message:"冷漠"
         }
+    },
+    
+    render(){
+        return <div>我该如何如何如何的</div>;
     }
-    // ,
-    // render(){
-    //     return <div>我该如何如何如何的{this.message}</div>
-    // }
 }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
 const path = require('path');
 
-function resolve(dir){
-    return path.join(__dirname,dir);
-}
-
 module.exports = {
     pages: {
         index: {// 构建应用入口
@@ -30,20 +26,21 @@ module.exports = {
     },
     chainWebpack: config => {
         // 配置路径别名
-        config.resolve.alias.set('@',resolve('./src'))
-        .set('~img',resolve('./src/assets/img'))
+        config.resolve.alias.set('@',path.join(__dirname,'./src'))
+        .set('~img',path.join(__dirname,'./src/assets/img'))
 
         // 解决ie11兼容ES6
         config.entry('main').add('babel-polyfill')
 
-        config.module
-            .rule('js')
-            .include.add(path.resolve(__dirname,'packages')).end()
-            .use('babel')
-            .loader('babel-loader')
-            .tap(options => {
-                return options
-            })
+        // 构建模块是配置的config.module会与jsx模块依赖冲突
+        // config.module
+        //     .rule('js')
+        //     .include.add(path.resolve(__dirname,'./packages')).end()
+        //     .use('babel')
+        //     .loader('babel-loader')
+        //     .tap(options => {
+        //         return options
+        //     })
     },
     transpileDependencies: [
         'biyi-admin', // 指定对第三方依赖包进行babel-polyfill处理
